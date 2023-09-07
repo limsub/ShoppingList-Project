a066229dd04ed23464cf61028428fbadbc919e5a
limsub <limsss9076@naver.com>
2023-09-07 17:38:28 +0900

    git ignore

---
+++ ShoppingList Project/Network/About.swift
@@ -0,0 +1,36 @@
+//
+//  About.swift
+//  ShoppingList Project
+//
+//  Created by 임승섭 on 2023/09/07.
+//
+
+import Foundation
+
+
+// MARK: - Shopping
+struct Shopping: Codable {
+    let lastBuildDate: String
+    let total, start, display: Int
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let title: String
+    let link: String
+    let image: String
+    let lprice, hprice, mallName, productID: String
+    let productType: String
+    let brand, maker: Brand
+    let category1: Category1
+    let category2: Category2
+    let category3: Category3
+    let category4: Category4
+
+    enum CodingKeys: String, CodingKey {
+        case title, link, image, lprice, hprice, mallName
+        case productID = "productId"
+        case productType, brand, maker, category1, category2, category3, category4
+    }
+}
